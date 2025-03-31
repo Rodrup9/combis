14da57b4f1f945e605c8a9407df8b3ba93a73a8b
LuisPerez <leperezmontejo@gmail.com>
2025-03-30 20:19:52 -0600

    paradas

--- src/paradas/entities/parada.entity.ts
+++ src/paradas/entities/parada.entity.ts
@@ -1,1 +1,21 @@
-export class Parada {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Parada {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text',)
+    nombre: string;
+
+    @Column('decimal', { precision: 10, scale: 6 })
+    latitud: number;
+
+    @Column('decimal', { precision: 10, scale: 6 })
+    longitud: number;
+
+    
+
+    
+}
--- src/paradas/paradas.module.ts
+++ src/paradas/paradas.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ParadasService } from './paradas.service';
 import { ParadasController } from './paradas.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Parada } from './entities/parada.entity';
 
 @Module({
   controllers: [ParadasController],
   providers: [ParadasService],
+  imports:[TypeOrmModule.forFeature([Parada])]
 })
 export class ParadasModule {}
